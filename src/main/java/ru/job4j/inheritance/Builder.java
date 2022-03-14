47403cc949be1f4b6c692f56e913a88b14a4c859
Samsung <tat-milakina@yandex.ru>
2022-03-14 21:57:37 +0300

    3. Реализация профессий в коде [#6837 #274886]

--- src/main/java/ru/job4j/inheritance/Builder.java
+++ src/main/java/ru/job4j/inheritance/Builder.java
@@ -2,7 +2,7 @@ package ru.job4j.inheritance;
 
 public class Builder extends Engineer {
 
-    Project project;
+    private Project project;
 
     public Builder(String name, String surname, String education, String birthday, String college) {
         super(name, surname, education, birthday, college);
