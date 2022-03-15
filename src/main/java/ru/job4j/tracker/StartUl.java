d2ca0d9b31f9af25afafa7e0e38b43feb9a67d9e
Samsung <tat-milakina@yandex.ru>
2022-03-15 21:20:19 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -4,12 +4,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUl {
-
     public static void main(String[] args){
         Item obj = new Item();
         LocalDateTime currentDateTime = obj.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(currentDateTime.format(formatter));
     }
-
 }
