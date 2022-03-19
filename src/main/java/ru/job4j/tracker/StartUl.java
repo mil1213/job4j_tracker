c3d4a7ead582235c1b2d56ea30c22e6689ffeb97
Samsung <tat-milakina@yandex.ru>
2022-03-19 22:47:58 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -5,9 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUl {
     public static void main(String[] args) {
-        Item obj = new Item();
+        Item obj = new Item(123, "Kosarev");
         LocalDateTime currentDateTime = obj.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(currentDateTime.format(formatter));
+        System.out.println(obj);
     }
 }
