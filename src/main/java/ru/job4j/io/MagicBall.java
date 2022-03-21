82f2db64897d9f8a25634a64eef07a39c6f60086
Samsung <tat-milakina@yandex.ru>
2022-03-21 22:17:17 +0300

    1. Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Задай мне закрытый вопрос.");
+        String question = input.nextLine();
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case 0 : System.out.println("Да");
+            break;
+            case 1 : System.out.println("Нет");
+                break;
+            default : System.out.println("Все возможно");
+        }
+    }
+}
