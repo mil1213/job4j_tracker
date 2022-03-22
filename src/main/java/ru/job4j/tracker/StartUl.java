d922b80df87a271a2f9594a23bdd60f96caa2d4f
Samsung <tat-milakina@yandex.ru>
2022-03-22 22:39:37 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -10,9 +10,14 @@ public class StartUl {
             showMenu();
             System.out.println("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select != 6) {
-                System.out.println("User chose: " + select);
-            } else {
+            if (select == 0) {
+                System.out.println("=== Creat new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println(" Item is added: " + item);
+            } else if (select == 6) {
                 run = false;
             }
         }
