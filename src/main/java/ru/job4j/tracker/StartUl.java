9c609c41da4b0ae74f576a0db916237477c40c3a
Samsung <tat-milakina@yandex.ru>
2022-03-23 22:17:06 +0300

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -48,6 +48,16 @@ public class StartUl {
                 } else {
                     System.out.println("Item's delete error");
                 }
+            } else if (select == 4) {
+                System.out.println("=== Find item by id ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Item with id = " + id + " is not found");
+                }
             }
             if (select == 6) {
                 run = false;
