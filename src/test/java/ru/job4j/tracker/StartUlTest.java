1f6445029c8f6af61d18b52ea700d903e22cf9ca
Samsung <tat-milakina@yandex.ru>
2022-03-27 16:21:07 +0300

    5. Input и полиморфизм. [#181094]

---
+++ src/test/java/ru/job4j/tracker/StartUlTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StartUlTest {
+    @Test
+    public void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        StartUl.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item exp = new Item("Fix PC") ;
+        assertThat(created.getName(), is(exp.getName()));
+    }
+}
