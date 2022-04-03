940d77f0ad6d0e28ffbc9cec5a86bee4bf96d43b
Samsung <tat-milakina@yandex.ru>
2022-04-03 21:35:26 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNLessThanNull() {
+        Fact.calc(-2);
+    }
+}
