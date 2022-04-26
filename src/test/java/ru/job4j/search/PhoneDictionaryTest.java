d6140dbefe104c1e1e1b21ce92f44039356b2992
Samsung <tat-milakina@yandex.ru>
2022-04-26 22:31:27 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Pet");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNothingFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("451");
+        assertThat(persons.isEmpty(), is(true));
+    }
+}
