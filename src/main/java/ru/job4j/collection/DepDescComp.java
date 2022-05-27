f68983a74e75aec52829497435770b2419f04436
Samsung <tat-milakina@yandex.ru>
2022-05-27 21:58:18 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl = o2.substring(0, 2).compareTo(o1.substring(0, 2));
+        int rsl = o2.split("/")[0].compareTo(o1.split("/")[0]);
         if (rsl == 0) {
-           rsl = o1.substring(2).compareTo(o2.substring(2));
+            rsl = o1.compareTo(o2);
         }
         return rsl;
     }
