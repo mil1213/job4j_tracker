f2309f06cf7be1243df3df05d15fdbaa453bcc44
Samsung <tat-milakina@yandex.ru>
2022-06-07 21:44:51 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -34,4 +34,8 @@ public class Address {
     public int hashCode() {
         return Objects.hash(city, street, home, apartment);
     }
+
+    public String getCity() {
+        return city;
+    }
 }
